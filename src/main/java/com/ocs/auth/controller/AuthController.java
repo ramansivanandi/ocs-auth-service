4e26622097360b842feeeaf369a81a951ace7073
ramansivanandi <ramfutures@gmail.com>
2023-06-02 17:53:02 +0300

    Update AuthController.java

--- src/main/java/com/ocs/auth/controller/AuthController.java
+++ src/main/java/com/ocs/auth/controller/AuthController.java
@@ -6,12 +6,16 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -87,6 +91,48 @@ public class AuthController {
 
 		Flux<String> flux = Flux.fromArray(new String[] { "A", "B", name });
 		return flux;
+
+	}
+	@GetMapping("/print")
+    public ResponseEntity<String> downloadExcel(@RequestParam("testParam")String param){
+        if(param.equals("badRequest")){
+            return new ResponseEntity<>("bad request",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Success",HttpStatus.OK);
+    }
+	
+	@RequestMapping(value = "/statusFlux", method = RequestMethod.POST)
+
+	public Flux<Map<String, Object>> postFluxServiceStatus(ServerHttpRequest request, ServerHttpResponse respons,
+			@RequestBody String body) {
+
+		System.out.println("Inside auth status method");
+
+		Map<String, Object> mapData = new HashMap<String, Object>();
+
+		mapData.put("body", body);
+		Flux<Map<String, Object>> fluxdata = Flux.just(mapData);
+
+		Gson gsonData = new Gson();
+
+		String json = gsonData.toJson(mapData);
+
+		String jsonData = "";
+		try {
+			// jsonData = new ObjectMapper().writeValueAsString(mapData);
+
+			ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+			jsonData = mapper.writeValueAsString(mapper.readTree(json));
+			log.info("gsonData:{}", jsonData);
+
+		} catch (JsonProcessingException e) {
+
+			e.printStackTrace();
+		}
+
+		System.out.println("Exit auth status method");
+
+		return fluxdata;
 
 	}
 
